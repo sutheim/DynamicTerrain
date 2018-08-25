19c766f19e208d77ab4682b2a645fda6090fc8d7
sutheim <41074166+sutheim@users.noreply.github.com>
2018-08-25 14:17:58 +0100

    Commit of everything actually working to a degree

--- Source/DynamicTerrain/DynamicTerrainComponent.cpp
+++ Source/DynamicTerrain/DynamicTerrainComponent.cpp
@@ -5,14 +5,18 @@
 
 UDynamicTerrainComponent::UDynamicTerrainComponent()
 {
-	//construction being done in BP for now
+	//InitializeTerrainArray();
 }
 
-void UDynamicTerrainComponent::initializeTerrainArray()
+void UDynamicTerrainComponent::InitializeTerrainArray()
 {
 	//Initialize landscape mesh with some noise
 
-	UE_LOG(LogTemp, Log, TEXT("Initializing mesh with resolution: %d"), terrainResolution);
+	UE_LOG(LogTemp, Log, TEXT("Initializing mesh with resolution: %d, terrainVertices stored at: %d"), terrainResolution, terrainVerticesPtr);
+
+
+	//check this badboi
+	check(terrainVerticesPtr);
 
 	//loop through the rows and collumns according to the resolution ( will always be square )
 	for (int yIndex = 0; yIndex < terrainResolution; yIndex = yIndex + 1) {
@@ -24,6 +28,8 @@ void UDynamicTerrainComponent::initializeTerrainArray()
 
 			float xCoord = xIndex * distanceBetweenPoints;
 			float yCoord = yIndex * distanceBetweenPoints;
+
+
 
 			//Add the vertices with some simple noise added to the height
 			terrainVertices.Add(FVector(xCoord, yCoord, SimplexNoise::sNoise(xCoord * terrainNoiseScale, yCoord * terrainNoiseScale) * terrainHeight));
@@ -55,19 +61,24 @@ void UDynamicTerrainComponent::initializeTerrainArray()
 
 
 	Super::CreateMeshSection(0,terrainVertices,terrainTriangles,terrainNormals,terrainUV0,terrainVertexColors,terrainTangents,true);
-	UE_LOG(LogTemp, Log, TEXT("terrainVertices array length is: %d after creation"), terrainVertices.Num());
+	UE_LOG(LogTemp, Log, TEXT("terrainVertices array length is: %d after creation, with a memory location of: %d"), terrainVertices.Num(), &terrainVertices);
+
 }
 
 void UDynamicTerrainComponent::ModifyTerrain(FVector location, float radius, float intensity)
 {
-	int xIndex = FMath::RoundHalfFromZero(location[0] / terrainWidth);
-	int yIndex = FMath::RoundHalfFromZero(location[1] / terrainWidth);
+	int xIndex = FMath::RoundHalfFromZero((location[0] / terrainWidth) * terrainResolution);
+	int yIndex = FMath::RoundHalfFromZero((location[1] / terrainWidth) * terrainResolution);
 
 	int32 index = terrainResolution * yIndex + xIndex;
+	
+	UE_LOG(LogTemp, Log, TEXT("Index of hit: %d. Based on xIndex: %d, and yIndex %d"), index,xIndex,yIndex);
 
-	UE_LOG(LogTemp, Log, TEXT("terrainVertices array length is: %d"), terrainVertices.Num());
+	UE_LOG(LogTemp, Log, TEXT("terrainVertices array length is: %d, with a memory location of: %d"), terrainVertices.Num(), &terrainVertices);
 
-	//terrainVertices.Insert(FVector(terrainVertices[index].X, terrainVertices[index].Y, terrainVertices[index].Z - 25.0f), index);
+	FVector newVector = FVector(terrainVertices[index].X, terrainVertices[index].Y, terrainVertices[index].Z - 25.0f);
 
-	//Super::CreateMeshSection(0, terrainVertices, terrainTriangles, terrainNormals, terrainUV0, terrainVertexColors, terrainTangents, true);
+	terrainVertices[index] = newVector;
+
+	Super::CreateMeshSection(0, terrainVertices, terrainTriangles, terrainNormals, terrainUV0, terrainVertexColors, terrainTangents, true);
 }
--- Source/DynamicTerrain/DynamicTerrainComponent.h
+++ Source/DynamicTerrain/DynamicTerrainComponent.h
@@ -19,32 +19,30 @@ class DYNAMICTERRAIN_API UDynamicTerrainComponent : public UProceduralMeshCompon
 public:
 	UDynamicTerrainComponent();
 
-	
+	UFUNCTION(BlueprintCallable)
+		void InitializeTerrainArray();
+
+	UFUNCTION(BlueprintCallable)
+		void ModifyTerrain(FVector location, float radius, float intensity);
+
+	UPROPERTY(EditAnywhere)
+		float terrainHeight = 64;
+
+	UPROPERTY(EditAnywhere)
+		int terrainResolution = 128;
+
+	UPROPERTY(EditAnywhere)
+		float terrainWidth = 4096;
+
+	UPROPERTY(EditAnywhere)
+		float terrainNoiseScale = 0.0008;
 
 private:
-	
 	TArray<FVector> terrainVertices;
+	TArray<FVector>* terrainVerticesPtr = &terrainVertices;
 	TArray<int32> terrainTriangles;
 	TArray<FVector> terrainNormals;
 	TArray<FVector2D> terrainUV0;
 	TArray<FColor> terrainVertexColors;
 	TArray<FProcMeshTangent> terrainTangents;
-
-	UPROPERTY(EditAnywhere)
-	float terrainHeight = 64;
-
-	UPROPERTY(EditAnywhere)
-	int terrainResolution = 128;
-	
-	UPROPERTY(EditAnywhere)
-	float terrainWidth = 4096;
-
-	UPROPERTY(EditAnywhere)
-	float terrainNoiseScale = 0.0008;
-	
-	UFUNCTION(BlueprintCallable)
-	void initializeTerrainArray();
-
-	UFUNCTION(BlueprintCallable)
-	void ModifyTerrain(FVector location,float radius, float intensity);
 };
